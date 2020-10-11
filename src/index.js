eb9f04dd539e3d0832556c7b2b59e02d6f837d0c
shelly-cx <584791842@qq.com>
2020-10-11 18:03:08 +0800

    '搞事情'

--- src/index.js
+++ src/index.js
@@ -6,12 +6,9 @@ import './assets/css/base.css'
 //约束字体比值
 import  './assets/js/font';
 
-//引入数据交互的插件配置
-import './plugins/axios';
-import './plugins/umi-request';
-import UcCellTest from "./components/uc-cell/uc-cell.test";
-
 import {BrowserRouter as Router} from 'react-router-dom';
+
+console.log('搞事情')
 
 ReactDOM.render(
     <Router>
