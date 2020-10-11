8c4a0068c7d1587f8c4c42f87904899e968cd4ed
shelly-cx <584791842@qq.com>
2020-10-11 18:07:06 +0800

    'leader修改了index'

--- src/index.js
+++ src/index.js
@@ -9,13 +9,18 @@ import  './assets/js/font';
 //引入数据交互的插件配置
 import './plugins/axios';
 import './plugins/umi-request';
-import UcCellTest from "./components/uc-cell/uc-cell.test";
+// import UcCellTest from "./components/uc-cell/uc-cell.test";
 
 import {BrowserRouter as Router} from 'react-router-dom';
 
+const show = () =>{
+    console.log('leader')
+};
+
 ReactDOM.render(
     <Router>
-        <UcCellTest/>
+        {/*<UcCellTest/>*/}
+        <div>hello</div>
     </Router>,
   document.getElementById('root')
 );
