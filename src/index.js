8cc54e810beb3be2caa2f170f9e106728f5d05f1
shelly-cx <584791842@qq.com>
2020-10-11 19:13:22 +0800

    '坑人,不过解决啦'

--- src/index.js
+++ src/index.js
@@ -6,24 +6,19 @@ import './assets/css/base.css'
 //约束字体比值
 import  './assets/js/font';
 
-
 //引入数据交互的插件配置
 import './plugins/axios';
 import './plugins/umi-request';
-// import UcCellTest from "./components/uc-cell/uc-cell.test";
+
 
 import {BrowserRouter as Router} from 'react-router-dom';
 
 const show = () =>{
     console.log('leader')
-};
-
-import {BrowserRouter as Router} from 'react-router-dom';
-
+}
 
 ReactDOM.render(
     <Router>
-        {/*<UcCellTest/>*/}
         <div>hello</div>
     </Router>,
   document.getElementById('root')
